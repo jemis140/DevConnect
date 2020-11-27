ca9214e8accf57d204cb740db151646530e57132
jemis140 <jemiszadafiya@gmail.com>
2020-11-27 23:12:43 +0530

    profile section completes

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-// const users = require('./routes/api/users');
-// const profile = require('./routes/api/profile');
-// const posts = require('./routes/api/posts');
+const connectDB = require('./config/db.js')
 
 const app = express();
 
-// DB Config
-// const db = require('./config/keys').mongoURI;
+connectDB();
 
-// Connect to MongoDB
-// mongoose
-//   .connect(db)
-//   .then(() => console.log('MongoDB Connected'))
-//   .catch(err => console.log(err));
+//init Middleware
+
+app.use(express.json({extended : false}));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-// // Use Routes
-// app.use('/api/users', users);
-// app.use('/api/profile', profile);
-// app.use('/api/posts', posts);
+//define routes
 
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
