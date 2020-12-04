d8834af3ed685bdc1fa7a111dc21f12c79d60867
jemis140 <jemiszadafiya@gmail.com>
2020-12-04 21:39:14 +0530

    Dashboard and profile management done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
+
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+
+import PrivateRoute from './components/routing/PrivateRouter';
+
 
 // Redux
 import { Provider } from 'react-redux';
+import { loadUser } from './actions/auth'; 
+import setAuthToken from './utils/setAuthToken';
+import { LOGOUT } from './actions/types';
 import store from './store';
 import './App.css';
+import { addEducation } from './actions/profile';
+
+
 
 
 const App = () => {
-  return (
+ 
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
+  return ( 
     <Provider store={store}>
         <Router>  
           <Navbar />
@@ -22,6 +52,12 @@ const App = () => {
             <Switch>
               <Route exact path="/Register" component={Register} />
               <Route exact path="/Login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              <PrivateRoute exact path="/add-education" component={AddEducation} />
+
             </Switch>
           </section>
         </Router>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, {Fragment, useState} from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -21,9 +22,14 @@ const Register = ({setAlert}) => {
         if (password !== password2) {
           setAlert('password not matched','danger');
         } else {
-            console.log('Success');
+            register({ name, email, password });
         }
     };
+
+    //Redirect if registered
+    if(isAuthenticated){
+        return <Redirect to="/dashboard" />
+    }
 
     return (
         <Fragment>
@@ -82,6 +88,12 @@ const Register = ({setAlert}) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
-  };
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
 
-export default connect(null,{setAlert})(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps,{setAlert, register})(Register);
